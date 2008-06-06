7407b8c8d5342107ca29c0ca7a23e95c5fb80d47
Aemon Cannon <aemoncannon@gmail.com>
2008-06-06 19:24:45 -0400

    Selectively ignore certain error statements.

--- test_helper.rb
+++ test_helper.rb
@@ -19,14 +19,16 @@ require 'find'
 require 'platform'
 require 'popen4'
 
-def run_tests(dir, testp, cmd_func)
+def run_tests(dir, testp, cmd_func, ignore_error = nil)
   Find.find(dir) { | path |
     if testp[path]
       puts "\n********* #{path} ***********"
       status = POpen4::popen4(cmd_func[path]) { | stdout, stderr, stdin, pid |
         stdout.readlines
         while o = stderr.gets
-          STDOUT.write o
+          if (not ignore_error) or (not ignore_error[o])
+            STDOUT.write o
+          end
         end
       }
       if status != 0
